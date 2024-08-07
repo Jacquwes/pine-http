24987699d8e2ae54549f8e057da1d314adfc7bde
Paul Hariel <paul.hariel@outlook.com>
2024-08-07 22:21:06 +0200

    Use winsock instead of asio

--- server/include/server_connection.h
+++ server/include/server_connection.h
@@ -4,30 +4,27 @@
 #include <mutex>
 #include <string_view>
 #include <thread>
-
-#include <asio/ip/tcp.hpp>
-
 #include "connection.h"
 #include "coroutine.h"
 #include "snowflake.h"
 
 namespace pine
 {
-    class server;
+  class server;
 
-    /// @brief A connection to a client.
-    class server_connection : public connection, public std::enable_shared_from_this<server_connection>
-    {
-    public:
-        /// @brief Construct a server connection with the given socket and server.
-        server_connection(asio::ip::tcp::socket& socket, server& server);
+  /// @brief A connection to a client.
+  class server_connection : public connection, public std::enable_shared_from_this<server_connection>
+  {
+  public:
+    /// @brief Construct a server connection with the given socket and server.
+    explicit server_connection(SOCKET socket);
 
-        /// @brief Start listening for messages from the client.
-        /// @return An asynchronous task completed when the connection has been closed.
+    /// @brief Start listening for messages from the client.
+    /// @return An asynchronous task completed when the connection has been closed.
     async_task start();
 
-    private:
-        /// @brief Whether the connection is connected.
-        bool is_connected = true;
-    };
+  private:
+    /// @brief Whether the connection is connected.
+    bool is_connected = true;
+  };
 }
--- server/src/server_connection.cpp
+++ server/src/server_connection.cpp
@@ -5,12 +5,9 @@
 #include <iostream>
 #include <random>
 
-#include <asio/ip/tcp.hpp>
 #include <connection.h>
 #include <coroutine.h>
-#include <message.h>
 #include <snowflake.h>
-#include <socket_messages.h>
 
 #include "server.h"
 #include "server_connection.h"
@@ -18,60 +15,17 @@
 
 namespace pine
 {
-	server_connection::server_connection(
-		asio::ip::tcp::socket& client_socket,
-		server& server
-	)
-		: server_ref{ server },
-		connection{ std::move(client_socket) }
-	{
+  server_connection::server_connection(SOCKET socket) : connection(socket)
+  {}
 
   async_task server_connection::start()
-	}
+  {
+    this->is_connected = true;
 
-	{
-		co_await switch_thread(listen_thread);
+    while (is_connected)
+    {
+    }
 
-		std::scoped_lock lock(connection_mutex);
-
-		for (auto& callback : server_ref.on_connection_attemps_callbacks)
-			callback(server_ref, server_connection::shared_from_this());
-
-		if (!(co_await establish_connection()))
-		{
-			close();
-			co_return;
-		}
-
-		std::cout << "[Server]   Client successfully connected: " << std::dec << id << std::endl;
-		is_connected = true;
-
-		for (auto& callback : server_ref.on_connection_callbacks)
-			callback(server_ref, shared_from_this());
-
-		while (is_connected)
-		{
-			auto&& message = co_await receive_message();
-
-			if (message->header.type == socket_messages::message_type::INVALID_MESSAGE)
-			{
-				close();
-				co_return;
-			}
-
-			server_ref.handle_message(shared_from_this(), message);
-		}
-	}
-
-		if (!(co_await identify()))
-		{
-			std::cout << "[Server]   Client failed identify: " << std::dec << id << std::endl;
-			co_return false;
-		}
-
-		std::cout << "[Server]   Client successfully identified in as \"" << user_data.username
-			<< "\": " << std::dec << id << std::endl;
-
-		co_return true;
-	}
-	}
+    co_return;
+  }
+}
