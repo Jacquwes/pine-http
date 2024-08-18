47cb2477d0e8e4fcdd60e12aee9046b698b5d22b
Paul Hariel <paul.hariel@outlook.com>
2024-08-18 21:18:29 +0200

    Adapt client example to drop asio

--- examples/client_example.cpp
+++ examples/client_example.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 
-#include <asio.hpp>
 #include <client.h>
 
 int main()
 {
-	asio::io_context context;
-	pine::client client("Example", context);
+  pine::client client;
 
-	if (client.connect("localhost"))
-	{
-		std::cout << "Connected to server" << std::endl;
-	}
-	else
-	{
-		std::cout << "Failed to connect to server" << std::endl;
-		return 1;
-	}
+  if (client.connect("localhost"))
+  {
+    std::cout << "Connected to server" << std::endl;
+  }
+  else
+  {
+    std::cout << "Failed to connect to server" << std::endl;
+    return 1;
+  }
 }
