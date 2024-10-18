abe3b6f152988805a26de99803d98a416f445d70
Paul Hariel <paul.hariel@outlook.com>
2024-10-19 00:19:00 +0200

    Improve example by demonstrating add_route

--- examples/server_example.cpp
+++ examples/server_example.cpp
@@ -8,6 +8,15 @@ int main()
 {
   pine::server server;
 
+  // Add a route that responds with "Hello, world!" to all requests at the root
+  server.add_route("/",
+                   [](const pine::http_request& request,
+                      pine::http_response& response)
+                   {
+                     response.set_status(pine::http_status::ok);
+                     response.set_body("Hello, world!");
+                   });
+
   if (auto server_result = server.start(); !server_result)
   {
     std::cerr << "Error: " << server_result.error().message() << std::endl;
