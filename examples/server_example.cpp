3f29563180e1f34341537ea3ef11285946738dff
Paul Hariel <paul.hariel@outlook.com>
2024-10-30 11:19:07 +0100

    Add comments to example

--- examples/server_example.cpp
+++ examples/server_example.cpp
@@ -9,7 +9,8 @@ int main()
 {
   pine::server server;
 
-  // Add a route that responds with "Hello, world!" to all requests at the root
+  // Add a route that responds to GET and HEAD requests to the root path.
+  // If the path is not valid, the compilation will fail.
   server.add_route("/",
                    [](const pine::http_request& request,
                       pine::http_response& response)
@@ -19,6 +20,7 @@ int main()
                    })
     .set_methods({ pine::http_method::get, pine::http_method::head });
 
+  // Add a route that responds to POST requests to the root path.
   server.add_route("/",
                    pine::http_method::post,
                    [](const pine::http_request& request,
@@ -29,9 +31,15 @@ int main()
                    });
 
 
+  // Add a route that responds to GET requests to /public_directory/*.
+  // This route will serve files from the public directory.
   server.add_static_route("/public_directory", std::filesystem::current_path() / "public");
+
+  // Add a route that responds to GET requests to /public_file.
+  // This route will serve the file about.html from the public directory.
   server.add_static_route("/public_file", std::filesystem::current_path() / "public" / "about.html");
 
+  // Start the server
   if (auto server_result = server.start(); !server_result)
   {
     std::cerr << "Error: " << server_result.error().message() << std::endl;
