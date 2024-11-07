064d04e28757103dd692f3e258d6ae5b6834fc07
Paul Hariel <paul.hariel@outlook.com>
2024-11-07 10:48:52 +0100

    Add all missing HTTP methods

--- shared/include/http.h
+++ shared/include/http.h
@@ -9,12 +9,38 @@
 namespace pine
 {
   /// @brief Represents an HTTP method.
+  /// More information at https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods.
   enum class http_method
   {
-    get, /// The GET method.
-    head, /// The HEAD method.
-    post, /// The POST method.
+    /// The GET method requests a representation of the specified resource. 
+    /// Requests using GET should only retrieve data and should not contain a
+    /// request content.
+    get = 1,
+    /// The HEAD method asks for a response identical to that of a GET request,
+    /// but without the response body.
+    head,
+    /// The POST method is used to submit an entity to the specified resource, 
+    /// often causing a change in state or side effects on the server.
+    post,
+    /// The PUT method replaces all current representations of the target 
+    /// resource with the request payload.
+    put,
+    /// The DELETE method deletes the specified resource.
+    delete_,
+    /// The CONNECT method establishes a tunnel to the server identified by the
+    /// target resource.
+    connect,
+    /// The OPTIONS method is used to describe the communication options for 
+    /// the target resource.
+    options,
+    /// The TRACE method performs a message loop-back test along the path to
+    /// the target resource.
+    trace,
+    /// The PATCH method is used to apply partial modifications to a resource.
+    patch,
   };
+
+  constexpr auto http_method_count = 9;
 
   /// @brief Map of HTTP methods to their string representations.
   inline const std::map<http_method, std::string> http_method_strings
